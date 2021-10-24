63656a2687e191afedc3c361d45d2b43245f4fff
kribow21 <kristininnotech@gmail.com>
2021-10-24 12:36:12 -0600

    setup project

---
+++ run.py
@@ -0,0 +1,22 @@
+from journal import app
+import sys
+
+if(len(sys.argv) > 1):
+    mode = sys.argv[1]
+    if(mode == "production"):
+        import bjoern
+        host = "0.0.0.0"
+        port = 5001
+        print("server is running in production code")
+        bjoern.run(app,host, port )
+    elif(mode == "testing"):
+        from flask_cors import CORS
+        CORS(app)
+        print("server is running in testing mode, switch to production when needed")
+        app.run(debug=True)
+    else:
+        print("invalid mode arguments, exiting")
+        exit()
+else:
+    print("There was no argument provided")
+    exit()
