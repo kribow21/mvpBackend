1525d1669350f2fc4cab29e241b0f9b59f4e6610
kribow21 <kristininnotech@gmail.com>
2021-11-07 11:51:58 -0700

    setup image delete request

--- journal/images.py
+++ journal/images.py
@@ -71,3 +71,106 @@ def journalImages():
             print('connection closed')
         else:
             print('the connection never opened, nothing to close')
+
+    if request.method == "GET":
+        try:
+            conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM image")
+            the_imgs = cursor.fetchall()
+            all_images = []
+            for img in the_imgs:
+                coll = {
+                    "imageId" : img[0],
+                    "imageURL" : img[1]
+                }
+                all_images.append(coll)
+            return Response(json.dumps(all_images, default=str),
+                            mimetype='application/json',
+                            status=201)
+        except mariadb.DatabaseError:
+            print('Something went wrong with connecting to database')
+        except mariadb.DataError: 
+            print('Something went wrong with your data')
+        except mariadb.OperationalError:
+            print('Something wrong with the connection')
+        except mariadb.ProgrammingError:
+            print('Your query was wrong')
+        except mariadb.IntegrityError:
+            print('Your query would have broken the database and we stopped it')
+        except mariadb.InterfaceError:
+            print('Something wrong with database interface')
+        except:
+            print('Something went wrong')
+        finally:
+            if(cursor != None):
+                cursor.close()
+                print('cursor closed')
+            else:
+                print('no cursor to begin with')
+            if(conn != None):   
+                conn.rollback()
+                conn.close()
+                print('connection closed')
+            else:
+                print('the connection never opened, nothing to close')
+
+    if request.method == "DELETE":
+        data = request.json
+        Etoken = data.get("editorToken")
+        imgID = data.get("imageId")
+        delete_fail = {
+            "message" : "something went wrong with deleting the image"
+        }
+        confirm = {
+            "message" : "image deleted"
+        }
+        data_error = {
+            "message" : "something wrong with passed data"
+        }
+        if (len(Etoken) == 32 and isinstance(imgID, int) == True):
+            try:
+                conn = mariadb.connect(user=dbcreds.user,password=dbcreds.password,host=dbcreds.host,port=dbcreds.port,database=dbcreds.database)
+                cursor = conn.cursor()
+                cursor.execute("SELECT editor_id FROM editor_session WHERE editor_token=?",[Etoken,])
+                session_editorID = cursor.fetchone()
+                if(len(session_editorID)==1):
+                    cursor.execute("DELETE from image WHERE id=?",[imgID])
+                    conn.commit()
+                    return Response(json.dumps(confirm, default=str),
+                                        mimetype="application/json",
+                                        status=200)
+                else:
+                    return Response(json.dumps(delete_fail, default=str),
+                                        mimetype="application/json",
+                                        status=400)
+            except mariadb.DatabaseError:
+                print('Something went wrong with connecting to database')
+            except mariadb.DataError: 
+                print('Something went wrong with your data')
+            except mariadb.OperationalError:
+                print('Something wrong with the connection')
+            except mariadb.ProgrammingError:
+                print('Your query was wrong')
+            except mariadb.IntegrityError:
+                print('Your query would have broken the database and we stopped it')
+            except mariadb.InterfaceError:
+                print('Something wrong with database interface')
+            except:
+                print('Something went wrong')
+            finally:
+                if(cursor != None):
+                    cursor.close()
+                    print('cursor closed')
+                else:
+                    print('no cursor to begin with')
+                if(conn != None):   
+                    conn.rollback()
+                    conn.close()
+                    print('connection closed')
+                else:
+                    print('the connection never opened, nothing to close')
+        else:
+            return Response(json.dumps(data_error, default=str),
+                                mimetype='application/json',
+                                status=401)
